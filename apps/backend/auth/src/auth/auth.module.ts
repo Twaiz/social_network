b7e691cf8df00a45ed7c79533d4e659fa84068cb
Twaiz <homenkobodya@gmail.com>
2025-04-09 14:02:51 +0300

    update[auth.module]: imports are configured in the auth module

--- apps/backend/auth/src/auth/auth.module.ts
+++ apps/backend/auth/src/auth/auth.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+// biome-ignore lint/style/useImportType: <explanation>
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { getMongoConfig } from '@backend-configs';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const mongoConfig: MongooseModuleOptions =
+          await getMongoConfig(configService);
+
+        if (!mongoConfig) {
+          throw new Error('❌ Произошла ошибка при подключение к БД!');
+        }
+
+        console.log('🚀 Проект успешно подключился к БД!');
+        return mongoConfig;
+      },
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
