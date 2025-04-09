087fc95d97dae4af6c29bfa0ed14345eabdf10eb
Twaiz <homenkobodya@gmail.com>
2025-04-09 15:18:47 +0300

    update[main]: improved code; added log message variables and more

---
+++ apps/backend/auth/src/main.constants.ts
@@ -0,0 +1,3 @@
+export const SERVER_CONNECTION_SUCCESS = '🚀 Сервер успешно запущен';
+export const SERVER_CONNECTION_FAILED =
+  '❌ Произошла ошибка при запуске сервера!';
--- apps/backend/auth/src/main.ts
+++ apps/backend/auth/src/main.ts
@@ -1,17 +1,31 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
+import {
+  SERVER_CONNECTION_FAILED,
+  SERVER_CONNECTION_SUCCESS,
+} from './main.constants';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
-
-  const globalPrefix = 'api';
+  const host = process.env.SERVER_HOST || 'localhost';
+  const globalPrefix = process.env.SERVER_GLOBAL_PREFIX || 'api';
   const port = process.env.SERVER_PORT || 3000;
 
-  app.setGlobalPrefix(globalPrefix);
-  await app.listen(port);
+  try {
+    const app = await NestFactory.create(AuthModule);
 
-  Logger.log(`🚀 Проект запущен: http://localhost:${port}/${globalPrefix}`);
+    app.setGlobalPrefix(globalPrefix);
+    await app.listen(port);
+
+    Logger.log(
+      `${SERVER_CONNECTION_SUCCESS}: http://${host}:${port}/${globalPrefix}`,
+    );
+  } catch (error) {
+    if (!(error instanceof Error)) return;
+
+    Logger.error(SERVER_CONNECTION_FAILED, error.stack, 'Bootstrap');
+    process.exit(1);
+  }
 }
 
 bootstrap();
