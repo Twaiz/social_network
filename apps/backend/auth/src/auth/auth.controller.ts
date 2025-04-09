5cde043e75f6860653236ac3442208e2a72c2c6c
Twaiz <homenkobodya@gmail.com>
2025-04-09 14:37:22 +0300

    update[auth.controller, auth.service]: added basic getMessage method for auth-e2e:test

--- apps/backend/auth/src/auth/auth.controller.ts
+++ apps/backend/auth/src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+// biome-ignore lint/style/useImportType: <explanation>
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get()
+  getMessage(): { message: string } {
+    return this.authService.getMessage();
+  }
+}
