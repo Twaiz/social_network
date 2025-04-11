28113587108d6ad61c18b1e85bfd087bc462f896
Twaiz <homenkobodya@gmail.com>
2025-04-11 22:13:20 +0300

    update[getJwtConfig]: added jwt token expiration date

--- libs/backend-configs/src/lib/config.constants.ts
+++ libs/backend-configs/src/lib/config.constants.ts
@@ -1,1 +1,2 @@
 export const JWT_SECRET_ERROR = '❌ Не найден JWT_SECRET код!';
+export const JWT_EXPIRES_IN_ERROR = '❌ Не найден JWT_EXPIRES_IN код!';
--- libs/backend-configs/src/lib/getJwtConfig.ts
+++ libs/backend-configs/src/lib/getJwtConfig.ts
@@ -3,14 +3,25 @@ import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 // biome-ignore lint/style/useImportType: <explanation>
 import { JwtModuleOptions } from '@nestjs/jwt';
-import { JWT_SECRET_ERROR } from './config.constants';
+import { JWT_EXPIRES_IN_ERROR, JWT_SECRET_ERROR } from './config.constants';
 
 export const getJwtConfig = (
   configService: ConfigService,
 ): JwtModuleOptions => {
   const secret: string | undefined = configService.get('JWT_SECRET');
+  const jwtExpires: string | undefined = configService.get('JWT_EXPIRES_IN');
+
   if (!secret) {
     Logger.error(JWT_SECRET_ERROR, 'JwtService');
   }
-  return { secret };
+  if (!jwtExpires) {
+    Logger.error(JWT_EXPIRES_IN_ERROR, 'JwtService');
+  }
+
+  return {
+    secret,
+    signOptions: {
+      expiresIn: jwtExpires,
+    },
+  };
 };
