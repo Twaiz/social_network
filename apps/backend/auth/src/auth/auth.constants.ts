551c0d5702f7d835a66faeb9163b9bee7647c0a0
Twaiz <homenkobodya@gmail.com>
2025-04-14 21:16:37 +0300

    update[auth.constants]: added variable message for bad request

--- apps/backend/auth/src/auth/auth.constants.ts
+++ apps/backend/auth/src/auth/auth.constants.ts
@@ -1,3 +1,11 @@
 export const DB_CONNECTION_SUCCESS = '🚀 Проект успешно подключился к БД!';
 export const DB_CONNECTION_FAILED = '❌ Произошла ошибка при подключение к БД!';
+
 export const EMAIL_VALIDATION_ERROR = '❌ Неверный формат электронной почты!';
+
+export const USER_ALREADY_REGISTERED_WITH_EMAIL_AND_LOGIN =
+  '❌ Пользователь с таким email и login уже зарегистрирован.';
+export const USER_ALREADY_REGISTERED_WITH_EMAIL =
+  '❌ Пользователь с таким email уже зарегистрирован.';
+export const USER_ALREADY_REGISTERED_WITH_LOGIN =
+  '❌ Пользователь с таким login уже зарегистрирован.';
