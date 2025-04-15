6b64d1f63779698d26d011bf057131f6111eda6f
Twaiz <homenkobodya@gmail.com>
2025-04-15 16:42:25 +0300

    update[user.credentials.dto]: added DTO field checking

--- apps/backend/auth/src/auth/dto/user-credentials.dto.ts
+++ apps/backend/auth/src/auth/dto/user-credentials.dto.ts
@@ -1,10 +1,17 @@
-import { IsString } from 'class-validator';
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  LOGIN_MAX_LENGTH_ERROR,
+  LOGIN_MIN_LENGTH_ERROR,
+} from '../auth.constants';
 
 export class UserCredentialsDto {
   @IsString()
+  @IsEmail()
   email!: string;
 
   @IsString()
+  @MinLength(4, { message: LOGIN_MIN_LENGTH_ERROR })
+  @MaxLength(16, { message: LOGIN_MAX_LENGTH_ERROR })
   login!: string;
 
   @IsString()
