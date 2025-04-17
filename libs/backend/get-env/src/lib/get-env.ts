a2586b7d0420c818a13be3fbde1a1b7577a76bd9
Twaiz <homenkobodya@gmail.com>
2025-04-17 15:19:21 +0300

    update[get-env]: added methods getMongodbPassword, getMongodbLogin

--- libs/backend/get-env/src/lib/get-env.ts
+++ libs/backend/get-env/src/lib/get-env.ts
@@ -1,14 +1,20 @@
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
-import { EnvString } from '@types';
-import { JWT_EXPIRES_IN_ERROR, JWT_SECRET_ERROR } from '@backend-configs';
+type EnvString = string | undefined;
+
+const CONSTANTS = {
+  JWT_SECRET_ERROR: '❌ Не найден JWT_SECRET!',
+  JWT_EXPIRES_IN_ERROR: '❌ Не найден JWT_EXPIRES_IN!',
+  MONGODB_PASSWORD_ERROR: '❌ Не найден MONGODB_PASSWORD!',
+  MONGODB_LOGIN_ERROR: '❌ Не найден MONGODB_LOGIN!',
+};
 
 export const GetEnv = {
   getJwtSecret(configService: ConfigService): string {
     const secret: EnvString = configService.get('JWT_SECRET');
     if (!secret) {
-      Logger.error(JWT_SECRET_ERROR, 'GetEnvService');
+      Logger.error(CONSTANTS.JWT_SECRET_ERROR, 'GetEnv');
       process.exit(1);
     }
     return secret;
@@ -17,9 +23,27 @@ export const GetEnv = {
   getJwtExpiresIn(configService: ConfigService): string {
     const expiresIn: EnvString = configService.get('JWT_EXPIRES_IN');
     if (!expiresIn) {
-      Logger.error(JWT_EXPIRES_IN_ERROR, 'GetEnvService');
+      Logger.error(CONSTANTS.JWT_EXPIRES_IN_ERROR, 'GetEnv');
       process.exit(1);
     }
     return expiresIn;
+  },
+
+  getMongodbPassword(configService: ConfigService): string {
+    const mongodbPassword: EnvString = configService.get('MONGODB_PASSWORD');
+    if (!mongodbPassword) {
+      Logger.error(CONSTANTS.MONGODB_PASSWORD_ERROR, 'GetEnv');
+      process.exit(1);
+    }
+    return mongodbPassword;
+  },
+
+  getMongodbLogin(configService: ConfigService): string {
+    const mongodbLogin: EnvString = configService.get('MONGODB_LOGIN');
+    if (!mongodbLogin) {
+      Logger.error(CONSTANTS.MONGODB_LOGIN_ERROR, 'GetEnv');
+      process.exit(1);
+    }
+    return mongodbLogin;
   },
 };
