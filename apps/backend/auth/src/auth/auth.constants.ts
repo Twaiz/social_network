7bd281b9e42cc785f8312c64143fd0ba19e803b6
Twaiz <homenkobodya@gmail.com>
2025-04-21 20:46:50 +0300

    update[auth.constants]: added new constants

--- apps/backend/auth/src/auth/auth.constants.ts
+++ apps/backend/auth/src/auth/auth.constants.ts
@@ -16,3 +16,5 @@ export const USER_ALREADY_REGISTERED_WITH_EMAIL =
   '❌ Пользователь с таким email уже зарегистрирован.';
 export const USER_ALREADY_REGISTERED_WITH_LOGIN =
   '❌ Пользователь с таким login уже зарегистрирован.';
+export const BOTH_EMAIL_AND_LOGIN_ERROR =
+  '❌ Укажите только один параметр: email или login.';
