9ab03a82d2bc500f377770443a747e80471bd30b
Twaiz <homenkobodya@gmail.com>
2025-04-22 15:43:57 +0300

    update[roles.decorator]: added best typization

--- libs/backend/decorators/src/lib/roles/roles.decorator.ts
+++ libs/backend/decorators/src/lib/roles/roles.decorator.ts
@@ -1,6 +1,7 @@
 import { CustomDecorator, SetMetadata } from '@nestjs/common';
 
 import { ROLES_KEY } from './roles.constants';
+import { EUserRole } from '@enums';
 
-export const Roles = (...roles: string[]): CustomDecorator<string> =>
+export const Roles = (...roles: EUserRole[]): CustomDecorator<string> =>
   SetMetadata(ROLES_KEY, roles);
