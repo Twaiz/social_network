d0d6b4fa9c6f62d6d4790398efb257b9b2c075f8
Twaiz <homenkobodya@gmail.com>
2025-04-23 18:06:43 +0300

    update[two-fa.controller]: added method enable2FA and added dto enable2FA.dto

--- apps/backend/two-fa/src/two-fa/two-fa.controller.ts
+++ apps/backend/two-fa/src/two-fa/two-fa.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Logger, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Logger, Post, Req, UseGuards } from '@nestjs/common';
 
 import { TwoFaService } from './two-fa.service';
 
 import { JwtAuthGuard } from '@guards';
 import type { IExpressRequest } from '@interfaces';
+import { Enable2FADto } from './dto/enable2FA.dto';
 
 @Controller('two-fa')
 export class TwoFaController {
@@ -11,7 +12,7 @@ export class TwoFaController {
 
   @Post('generate')
   @UseGuards(JwtAuthGuard)
-  async generate(@Req() req: IExpressRequest): Promise<{
+  async generate2FASecret(@Req() req: IExpressRequest): Promise<{
     secret: string;
     otpauthUrl: string;
     qrCodeDataUrl: string;
@@ -23,5 +24,28 @@ export class TwoFaController {
     }
 
     return this.twoFaService.generateTwoFactorSecret(user);
+  }
+
+  @Post('enable')
+  @UseGuards(JwtAuthGuard)
+  async enable2FA(
+    @Req() req: IExpressRequest,
+    @Body() token: Enable2FADto,
+  ): Promise<{
+    status: string;
+    code: number;
+    message: string;
+  }> {
+    const { code } = token;
+    const user = req.user;
+
+    if (!user) {
+      Logger.error('Не найден User в Request');
+      process.exit(1);
+    }
+
+    console.log(code);
+
+    return this.twoFaService.enableTwoFactor(user, code);
   }
 }
