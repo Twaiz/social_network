8f77c10252791789178d6188f8dec953c3f2a604
Twaiz <homenkobodya@gmail.com>
2025-04-23 18:56:39 +0300

    update[enable2FA.dto]: moved to lib - dtos + rename to enable-2fa

--- libs/backend/dtos/src/index.ts
+++ libs/backend/dtos/src/index.ts
@@ -1,3 +1,4 @@
 export { LoginDto } from './lib/auth/login.dto';
 export { UserLoginCredentialsDto } from './lib/auth/user-login-credentials.dto';
 export { UserRegisterCredentialsDto } from './lib/auth/user-register-credentials.dto';
+export { Enable2FADto } from './lib/two-fa/enable-2FA.dto';
