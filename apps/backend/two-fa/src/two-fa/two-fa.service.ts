b409b9439d493896af66a3521a242f3cb9dd6705
Twaiz <homenkobodya@gmail.com>
2025-04-23 10:11:25 +0300

    update[two-fa.service]: added method generateTwoFactorSecret

--- apps/backend/two-fa/src/two-fa/two-fa.service.ts
+++ apps/backend/two-fa/src/two-fa/two-fa.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { authenticator } from 'otplib';
+import * as qrcode from 'qrcode';
+
+import { IUser } from '@interfaces';
 
 @Injectable()
-export class TwoFaService {}
+export class TwoFaService {
+  async generateTwoFactorSecret(user: IUser): Promise<{
+    secret: string;
+    otpauthUrl: string;
+    qrCodeDataUrl: string;
+  }> {
+    const serviceName = 'TwoFa';
+
+    const secret = authenticator.generateSecret();
+    const otpauthUrl = authenticator.keyuri(user.email, serviceName, secret);
+    const qrCodeDataUrl = await qrcode.toDataURL(otpauthUrl);
+
+    return {
+      secret,
+      otpauthUrl,
+      qrCodeDataUrl,
+    };
+  }
+}
