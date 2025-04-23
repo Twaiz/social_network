93c70be5345cb3997e73f4e09daa2e7a3422ebd0
Twaiz <homenkobodya@gmail.com>
2025-04-23 15:39:11 +0300

    update[user-login-credentials.dto]: moved user-login-credentials.dto from project auth to lib dtos

--- libs/backend/dtos/src/index.ts
+++ libs/backend/dtos/src/index.ts
@@ -1,2 +1,3 @@
 export { LoginDto } from './lib/auth/login.dto';
+export { UserLoginCredentialsDto } from './lib/auth/user-login-credentials.dto';
 export { UserRegisterCredentialsDto } from './lib/auth/user-register-credentials.dto';
