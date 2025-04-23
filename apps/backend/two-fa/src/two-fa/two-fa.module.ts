7e698b48d95bb2b46945ea6e97e38254346d5c45
Twaiz <homenkobodya@gmail.com>
2025-04-23 11:55:04 +0300

    update[two-fa.service]: added recording 2FA secret code in the database

--- apps/backend/two-fa/src/two-fa/two-fa.module.ts
+++ apps/backend/two-fa/src/two-fa/two-fa.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+
 import { TwoFaController } from './two-fa.controller';
 import { TwoFaService } from './two-fa.service';
+
+import { AuthService } from '@services';
 
 @Module({
   controllers: [TwoFaController],
   providers: [TwoFaService],
+  imports: [AuthService],
 })
 export class TwoFaModule {}
--- apps/backend/two-fa/src/two-fa/two-fa.service.ts
+++ apps/backend/two-fa/src/two-fa/two-fa.service.ts
@@ -3,19 +3,24 @@ import { authenticator } from 'otplib';
 import * as qrcode from 'qrcode';
 
 import { IUser } from '@interfaces';
+import { AuthService } from '@services';
 
 @Injectable()
 export class TwoFaService {
+  constructor(private readonly userService: AuthService) {}
+
   async generateTwoFactorSecret(user: IUser): Promise<{
     secret: string;
     otpauthUrl: string;
     qrCodeDataUrl: string;
   }> {
-    const serviceName = 'TwoFa';
+    const serviceName = 'SocialNetwork';
 
     const secret = authenticator.generateSecret();
     const otpauthUrl = authenticator.keyuri(user.email, serviceName, secret);
     const qrCodeDataUrl = await qrcode.toDataURL(otpauthUrl);
+
+    await this.userService.updateTwoFactorSecret(user._id, secret);
 
     return {
       secret,
