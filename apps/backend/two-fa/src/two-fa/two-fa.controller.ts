0b2456920c8fae054798a11df73c3f6546b3a1a9
Twaiz <homenkobodya@gmail.com>
2025-04-23 10:47:28 +0300

    update[two-fa.controller]: added method generate

--- apps/backend/two-fa/src/two-fa/two-fa.controller.ts
+++ apps/backend/two-fa/src/two-fa/two-fa.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger, Post, Req, UseGuards } from '@nestjs/common';
+import type { Request } from 'express';
+
+import { TwoFaService } from './two-fa.service';
+import { JwtAuthGuard } from '@guards';
 
 @Controller('two-fa')
-export class TwoFaController {}
+export class TwoFaController {
+  constructor(private readonly twoFaService: TwoFaService) {}
+
+  @Post('generate')
+  @UseGuards(JwtAuthGuard)
+  async generate(@Req() req: Request): Promise<{
+    secret: string;
+    otpauthUrl: string;
+    qrCodeDataUrl: string;
+  }> {
+    const user = req.user;
+    if (!user) {
+      Logger.error('Не найден User в Request');
+      process.exit(1);
+    }
+
+    return this.twoFaService.generateTwoFactorSecret(user);
+  }
+}
