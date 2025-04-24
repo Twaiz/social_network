f76168d01dd0a2bcb254b0d7034ebee950b9a2a4
Twaiz <homenkobodya@gmail.com>
2025-04-24 16:29:46 +0300

    update[rolesGuard]: moved to shared/guards

---
+++ libs/backend/shared/src/index.ts
@@ -0,0 +1,5 @@
+export * from './lib/enums/user.role';
+export * from './lib/interfaces/express-request.interface';
+export * from './lib/interfaces/user.interface';
+export * from './lib/guards/jwtAuth.guard';
+export * from './lib/guards/roles.guard';
