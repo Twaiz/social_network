a9ccbb1da617e212a6254bcc10c08277f78c793c
Twaiz <homenkobodya@gmail.com>
2025-04-24 16:50:22 +0300

    update[two-fa.service]: moved to two-fa-lib/service

--- libs/backend/services/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { AuthService } from './lib/auth/auth.service';
-export { TwoFaService } from './lib/two-fa/two-fa.service';
--- libs/backend/services/src/lib/two-fa/two-fa.constants.ts
+++
@@ -1,2 +0,0 @@
-export const INVALID_2FA_CODE = '❌ Неверный код 2FA';
-export const USER_NOT_FOUND = '❌ Такого пользователя не найдено';
