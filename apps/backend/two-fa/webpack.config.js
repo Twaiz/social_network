4aa2f20a0626c284fb0489de0314729ab513ab08
Twaiz <homenkobodya@gmail.com>
2025-04-25 16:09:45 +0300

    update[project - two-fa]: added alias @two-fa-lib

--- apps/backend/two-fa/webpack.config.js
+++ apps/backend/two-fa/webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
       '@configs': resolve(__dirname, '../../../libs/backend/configs/src'),
       '@enums': resolve(__dirname, '../../../libs/backend/enums/src'),
       '@dtos': resolve(__dirname, '../../../libs/backend/dtos/src'),
+      '@two-fa-lib': resolve(__dirname, '../../../libs/backend/two-fa-lib/src'),
     },
   },
   output: {
