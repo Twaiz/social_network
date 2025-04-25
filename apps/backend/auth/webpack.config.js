3974c1c7faaf42cbe849233aac5e702360894a22
Twaiz <homenkobodya@gmail.com>
2025-04-25 16:34:36 +0300

    update[project - auth tsconfig.app, webpack.config]: added new alias for fixed auth error

--- apps/backend/auth/webpack.config.js
+++ apps/backend/auth/webpack.config.js
@@ -5,22 +5,12 @@ module.exports = {
   resolve: {
     alias: {
       '@configs': resolve(__dirname, '../../../libs/backend/configs/src'),
-      '@interfaces': resolve(__dirname, '../../../libs/backend/interfaces/src'),
-      '@jwt-utils': resolve(__dirname, '../../../libs/backend/jwt-utils/src'),
-      '@enums': resolve(__dirname, '../../../libs/backend/enums/src'),
       '@get-env': resolve(__dirname, '../../../libs/backend/get-env/src'),
-      '@decorators': resolve(__dirname, '../../../libs/backend/decorators/src'),
-      '@guards': resolve(__dirname, '../../../libs/backend/guards/src'),
       '@bootstrap': resolve(__dirname, '../../../libs/backend/bootstrap/src'),
-      '@services': resolve(__dirname, '../../../libs/backend/services/src'),
-      '@dtos': resolve(__dirname, '../../../libs/backend/dtos/src'),
-      '@modules': resolve(__dirname, '../../../libs/backend/modules/src'),
-      '@controllers': resolve(
-        __dirname,
-        '../../../libs/backend/controllers/src',
-      ),
       '@models': resolve(__dirname, '../../../libs/backend/models/src'),
       '@auth-lib': resolve(__dirname, '../../../libs/backend/auth-lib/src'),
+      '@shared': resolve(__dirname, '../../../libs/backend/shared/src'),
+      '@two-fa-lib': resolve(__dirname, '../../../libs/backend/two-fa-lib/src'),
     },
   },
   output: {
