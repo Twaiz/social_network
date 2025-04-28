46788e72443c133724cd0daf48057f74023df6de
Twaiz <homenkobodya@gmail.com>
2025-04-28 19:13:05 +0300

    update[handlerControllers]: added select select to receive

--- libs/backend/shared/src/utils/handlerController.ts
+++ libs/backend/shared/src/utils/handlerController.ts
@@ -3,16 +3,18 @@ import { Model } from 'mongoose';
 const findUserByEmail = async <T>(
   model: Model<T>,
   email: string,
+  select?: string,
 ): Promise<T | null> => {
-  return model.findOne({ email });
+  return model.findOne({ email }).select(select || '');
 };
 
 //* Find User By Login *//
 const findUserByLogin = async <T>(
   model: Model<T>,
   login: string,
+  select?: string,
 ): Promise<T | null> => {
-  return model.findOne({ login });
+  return model.findOne({ login }).select(select || '');
 };
 
 export { findUserByEmail, findUserByLogin };
