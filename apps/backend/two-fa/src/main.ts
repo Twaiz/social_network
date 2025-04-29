588a6726842293c51f831af3a040a8ce6c17bc23
Twaiz <homenkobodya@gmail.com>
2025-04-29 20:13:51 +0300

    update[auth and two-fa main.ts]: added passing port to bootstrap

--- apps/backend/two-fa/src/main.ts
+++ apps/backend/two-fa/src/main.ts
@@ -1,4 +1,13 @@
+import { Logger } from '@nestjs/common';
+
 import { bootstrap } from '@bootstrap';
 import { TwoFaModule } from '@two-fa-lib';
+import { SERVER_PORT_NOT_FOUND } from '@shared';
 
-bootstrap(TwoFaModule);
+const port = Number(process.env.PORT);
+if (!port) {
+  Logger.log(SERVER_PORT_NOT_FOUND);
+  process.exit(1);
+}
+
+bootstrap(TwoFaModule, port);
