016ff53f8354a37e3a5d3e8d3c95b2625d270a52
Twaiz <homenkobodya@gmail.com>
2025-04-29 20:11:58 +0300

    update[bootstrap]: added accepting custom port

--- libs/backend/bootstrap/src/bootstrap.ts
+++ libs/backend/bootstrap/src/bootstrap.ts
@@ -11,19 +11,21 @@ type IEntryNestModule =
   | ForwardReference
   | Promise<IEntryNestModule>;
 
-export async function bootstrap(module: IEntryNestModule): Promise<void> {
+export async function bootstrap(
+  module: IEntryNestModule,
+  customPort: number,
+): Promise<void> {
   const host = process.env.SERVER_HOST || 'localhost';
   const globalPrefix = process.env.SERVER_GLOBAL_PREFIX || 'api';
-  const port = process.env.SERVER_PORT || 3000;
 
   try {
     const app = await NestFactory.create(module);
 
     app.setGlobalPrefix(globalPrefix);
-    await app.listen(port);
+    await app.listen(customPort);
 
     Logger.log(
-      `${SERVER_CONNECTION_SUCCESS}: http://${host}:${port}/${globalPrefix}`,
+      `${SERVER_CONNECTION_SUCCESS}: http://${host}:${customPort}/${globalPrefix}`,
     );
   } catch (error) {
     if (!(error instanceof Error)) return;
