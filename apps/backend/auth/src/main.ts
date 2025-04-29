48cc2c79dc4a8d8cbdf2d5217a11327e0cd6b6d0
Twaiz <homenkobodya@gmail.com>
2025-04-29 20:27:30 +0300

    update[auth, two-fa main.ts]: name constants port

--- apps/backend/auth/src/main.ts
+++ apps/backend/auth/src/main.ts
@@ -4,7 +4,7 @@ import { bootstrap } from '@bootstrap';
 import { AuthModule } from '@auth-lib';
 import { SERVER_PORT_NOT_FOUND } from '@shared';
 
-const port = Number(process.env.PORT);
+const port = Number(process.env.AUTH_SERVER_PORT);
 if (!port) {
   Logger.log(SERVER_PORT_NOT_FOUND);
   process.exit(1);
--- apps/backend/two-fa/src/main.ts
+++ apps/backend/two-fa/src/main.ts
@@ -4,7 +4,7 @@ import { bootstrap } from '@bootstrap';
 import { TwoFaModule } from '@two-fa-lib';
 import { SERVER_PORT_NOT_FOUND } from '@shared';
 
-const port = Number(process.env.PORT);
+const port = Number(process.env.TWO_FA_SERVER_PORT);
 if (!port) {
   Logger.log(SERVER_PORT_NOT_FOUND);
   process.exit(1);
