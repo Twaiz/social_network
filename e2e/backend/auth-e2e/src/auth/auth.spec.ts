d28a1a6881f54f30fe3cf93288bfe70afed13fb4
Twaiz <homenkobodya@gmail.com>
2025-04-30 21:22:29 +0300

    update[auth.spec]: configured

--- e2e/backend/auth-e2e/src/auth/auth.spec.ts
+++ e2e/backend/auth-e2e/src/auth/auth.spec.ts
@@ -1,12 +1,26 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { App } from 'supertest/types';
 import axios from 'axios';
+
+import { AuthModule } from '@auth-lib';
+import { disconnect } from 'node:process';
 
 const AUTH_ROUTE = '/auth';
 
-describe('GET /api', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(AUTH_ROUTE);
+describe('App - Auth (e2e)', () => {
+  let app: INestApplication<App>;
 
-    expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AuthModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await disconnect();
   });
 });
