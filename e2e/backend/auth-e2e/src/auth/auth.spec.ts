a8a172f04d704366c074834860dc9e429648be53
Twaiz <homenkobodya@gmail.com>
2025-04-30 21:44:07 +0300

    update[auth.spec]: added method register

--- e2e/backend/auth-e2e/src/auth/auth.spec.ts
+++ e2e/backend/auth-e2e/src/auth/auth.spec.ts
@@ -1,10 +1,19 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { disconnect } from 'node:process';
 import { App } from 'supertest/types';
 import axios from 'axios';
 
-import { AuthModule } from '@auth-lib';
-import { disconnect } from 'node:process';
+import { AuthModule, UserRegisterCredentialsDto } from '@auth-lib';
+import { IUser } from '@shared';
+
+const RegisterCredentials: UserRegisterCredentialsDto = {
+  email: 'twaiz@gmail.com',
+  login: 'twaiz',
+  password: '1205Qaz!',
+  firstName: 'Bohdan',
+  secondName: 'Twaiz',
+};
 
 const AUTH_ROUTE = '/auth';
 
@@ -18,6 +27,14 @@ describe('App - Auth (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+  });
+
+  it('auth/register -- success', async () => {
+    const res: axios.AxiosResponse<Promise<{ user: IUser; token: string }>> =
+      await axios.post(`${AUTH_ROUTE}/register`, RegisterCredentials);
+
+    expect(res.status).toBe(200);
+    expect(res.data).toEqual(Promise<{ user: IUser; token: string }>);
   });
 
   afterAll(async () => {
