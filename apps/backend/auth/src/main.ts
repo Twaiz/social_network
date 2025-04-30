cc931e8623f7356f39ea96b68830c8f4e5202190
Twaiz <homenkobodya@gmail.com>
2025-04-30 21:05:11 +0300

    update[auth main.ts]: updated arguments in function bootstrap

--- apps/backend/auth/src/main.ts
+++ apps/backend/auth/src/main.ts
@@ -1,13 +1,4 @@
-import { Logger } from '@nestjs/common';
-
 import { bootstrap } from '@bootstrap';
 import { AuthModule } from '@auth-lib';
-import { SERVER_PORT_NOT_FOUND } from '@shared';
 
-const port = Number(process.env.AUTH_SERVER_PORT);
-if (!port) {
-  Logger.error(SERVER_PORT_NOT_FOUND);
-  process.exit(1);
-}
-
-bootstrap(AuthModule, port);
+bootstrap(AuthModule, 'AUTH');
