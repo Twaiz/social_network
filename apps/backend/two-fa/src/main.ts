4646aa09cc73df2c32573d596e92b5fa4678a560
Twaiz <homenkobodya@gmail.com>
2025-04-30 21:05:23 +0300

    update[two-fa main.ts]: updated arguments in function bootstrap

--- apps/backend/two-fa/src/main.ts
+++ apps/backend/two-fa/src/main.ts
@@ -1,13 +1,4 @@
-import { Logger } from '@nestjs/common';
-
 import { bootstrap } from '@bootstrap';
 import { TwoFaModule } from '@two-fa-lib';
-import { SERVER_PORT_NOT_FOUND } from '@shared';
 
-const port = Number(process.env.TWO_FA_SERVER_PORT);
-if (!port) {
-  Logger.error(SERVER_PORT_NOT_FOUND);
-  process.exit(1);
-}
-
-bootstrap(TwoFaModule, port);
+bootstrap(TwoFaModule, 'TWO_FA');
