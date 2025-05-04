52c537bbc1223748ac6cbebc987b1cc479dade68
Twaiz <homenkobodya@gmail.com>
2025-05-04 19:47:45 +0300

    update[auth.controller]: updated method register - delete local use validationPipes

--- libs/backend/auth-lib/src/controllers/auth.controller.ts
+++ libs/backend/auth-lib/src/controllers/auth.controller.ts
@@ -8,8 +8,6 @@ import {
   Post,
   Req,
   UseGuards,
-  UsePipes,
-  ValidationPipe,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -53,7 +51,6 @@ export class AuthController {
   //* Register *//
   @HttpCode(201)
   @Post('register')
-  @UsePipes(new ValidationPipe())
   async register(
     @Body() userRegisterCredentialsDto: UserRegisterCredentialsDto,
   ): Promise<RegisterResponse> {
