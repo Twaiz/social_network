80482fecd61d5ae6a8cf9488386dfd5b21fe13a3
Twaiz <homenkobodya@gmail.com>
2025-05-04 16:40:52 +0300

    add[two-fa.spec]

---
+++ e2e/backend/two-fa-e2e/src/two-fa/two-fa.spec.ts
@@ -0,0 +1,35 @@
+import { INestApplication } from '@nestjs/common';
+import { App } from 'supertest/types';
+import request from 'supertest';
+
+import { TwoFaModule } from '@two-fa-lib';
+import { GetEnv } from '@get-env';
+import { bootstrap } from '@bootstrap';
+
+describe('App - TwoFa (e2e)', () => {
+  let app: INestApplication<App>;
+
+  beforeAll(async () => {
+    const customPort = process.env.AUTH_SERVER_PORT;
+    const port = GetEnv.getServerPort(customPort);
+
+    const serverApp = await bootstrap<App>(TwoFaModule, port);
+    if (!serverApp) {
+      process.exit(1);
+    }
+
+    app = serverApp;
+  });
+
+  it('two-fa/get-message', async () => {
+    const res = await request(app.getHttpServer())
+      .get('/api/two-fa/get-message')
+      .expect(200);
+
+    expect(res.status).toBe(200);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
