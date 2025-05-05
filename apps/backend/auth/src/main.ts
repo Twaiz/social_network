860808b9598ff3d1e13d393f7aa0d269ce928430
Twaiz <homenkobodya@gmail.com>
2025-05-05 16:10:10 +0300

    update[main.ts]: review

--- apps/backend/auth/src/main.ts
+++ apps/backend/auth/src/main.ts
@@ -2,7 +2,7 @@ import { bootstrap } from '@bootstrap';
 import { AuthModule } from '@auth-lib';
 import { GetEnv } from '@get-env';
 
-const customPort = process.env.AUTH_SERVER_PORT;
-const port = GetEnv.getServerPort(customPort);
+const authPort = process.env.AUTH_SERVER_PORT;
+const port = GetEnv.getServerPort(authPort);
 
 bootstrap<AuthModule>(AuthModule, port);
