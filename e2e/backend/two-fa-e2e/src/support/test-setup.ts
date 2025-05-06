7edfdd0d0afc56c2aab5f8a01b44f3ed1d8ec2ba
Twaiz <homenkobodya@gmail.com>
2025-05-06 17:46:29 +0300

    update[auth-e2e, two-fa-e2e]: removed folder 'support'

--- e2e/backend/two-fa-e2e/src/support/test-setup.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-import { GetEnv } from '@get-env';
-
-module.exports = async () => {
-  const { port, host, globalPrefix } =
-    GetEnv.getMongodbConnectionParametrs('TWO_FA');
-  axios.defaults.baseURL = `http://${host}:${port}/${globalPrefix}`;
-};
