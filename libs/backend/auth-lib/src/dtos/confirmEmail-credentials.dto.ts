8cb288cec572ed5d1ba09d9f77d2e9bdab1bf42b
Twaiz <homenkobodya@gmail.com>
2025-05-08 15:29:47 +0300

    update[confirmEmail-credentials.dto rename to confirmEmail-credentials.dto]

--- libs/backend/auth-lib/src/dtos/confirmEmail-credentials.dto.ts
+++ libs/backend/auth-lib/src/dtos/confirmEmail-credentials.dto.ts
@@ -1,6 +1,6 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
-export class ConfirmEmailDto {
+export class ConfirmEmailCredentialsDto {
   @IsString()
   @IsNotEmpty()
   token!: string;
