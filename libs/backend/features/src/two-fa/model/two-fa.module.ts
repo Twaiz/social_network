670cbc8fd98c471771de1d193748115647d35e04
Twaiz <homenkobodya@gmail.com>
2025-05-11 20:08:23 +0300

    update[two-fa.module]: updated paths and added TODO

--- libs/backend/features/src/two-fa/model/two-fa.module.ts
+++ libs/backend/features/src/two-fa/model/two-fa.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
+//TODO короче, хз что тут делать, т.к. у нас есть правила импортов в слоях. Нельзя, чтобы слой model что-то брал из слоя api та и вообще во всех других слоях. Хз, что делать
 import { TwoFaController } from '../controllers';
-import { TwoFaService } from '../services';
+import { TwoFaService } from './two-fa.service';
 
-import { connectToMongoDB } from '@configs';
-import { UserSchema } from '@models';
-import { JwtStrategy } from '@shared';
+import { connectToMongoDB, JwtStrategy } from '@shared';
+import { UserSchema } from '@entities/user';
 
 @Module({
   imports: [
