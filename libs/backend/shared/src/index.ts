5bc8fc80001d75e409ecdbeee957e67f61937896
Twaiz <homenkobodya@gmail.com>
2025-05-11 11:17:43 +0300

    update[lib - shared]: added index.ts

--- libs/backend/shared/src/index.ts
+++ libs/backend/shared/src/index.ts
@@ -1,8 +1,16 @@
-export * from './decorators';
-export * from './enums';
-export * from './guards';
-export * from './interfaces';
-export * from './strategies';
-export * from './constants';
-export * from './responses';
-export * from './types';
+export * from './config/connect-to-mongo/connectToMongoDB';
+export * from './config/constants/global.constants';
+export * from './config/jwt/getJwtConfig';
+
+export * from './kernel/lib/bootstrap/bootstrap';
+export * from './kernel/lib/get-env/get-env';
+
+export * from './structure/decorators/roles/roles.decorator';
+export * from './structure/enums/user.role';
+export * from './structure/guards/jwtAuth.guard';
+export * from './structure/guards/roles.guard';
+export * from './structure/interfaces/authenticated-request.interface';
+export * from './structure/interfaces/user.interface';
+export * from './structure/responses/login.response';
+export * from './structure/responses/register.response';
+export * from './structure/strategies/jwt.strategy';
