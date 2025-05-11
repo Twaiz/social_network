e0c545281480e1298610fddf802bb8617b9db21a
Twaiz <homenkobodya@gmail.com>
2025-05-11 19:54:21 +0300

    update[auth.module]: updated imports paths and added TODO

--- libs/backend/features/src/auth/model/auth.module.ts
+++ libs/backend/features/src/auth/model/auth.module.ts
@@ -7,10 +7,10 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthController } from '../api/auth.controller';
 import { AuthService } from './auth.service';
 
+//TODO хз что с этим делать, т.к. по правилам FSD слой auth не может ничего брать из слоя two-fa
 import { TwoFaModule } from '@two-fa-lib';
-import { UserSchema } from '@models';
-import { getJwtConfig, connectToMongoDB } from '@configs';
-import { JwtStrategy } from '@shared';
+import { UserSchema } from '@entities/user';
+import { JwtStrategy, getJwtConfig, connectToMongoDB } from '@shared';
 
 @Module({
   imports: [
