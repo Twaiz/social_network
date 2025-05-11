7e03a2ce59d57149abd29266498b7d3741dfbb49
Twaiz <homenkobodya@gmail.com>
2025-05-11 10:59:50 +0300

    update[decorators]: reorganized into folders

--- libs/backend/shared/src/structure/decorators/roles/roles.decorator.ts
+++ libs/backend/shared/src/structure/decorators/roles/roles.decorator.ts
@@ -1,7 +1,7 @@
 import { CustomDecorator, SetMetadata } from '@nestjs/common';
 
-import { ROLES_KEY } from '../../decorators';
-import { EUserRole } from '../enums/user.role';
+import { ROLES_KEY } from './constant/roles-decorator.constants';
+import { EUserRole } from '../../enums/user.role';
 
 export const Roles = (...roles: EUserRole[]): CustomDecorator<string> =>
   SetMetadata(ROLES_KEY, roles);
