2a213d7df38406ad95d9823ce5c2efc3becba9df
Twaiz <homenkobodya@gmail.com>
2025-05-11 09:49:20 +0300

    update[features/auth]: added global auth constants

--- libs/backend/features/src/auth/auth.constants.ts
+++ libs/backend/features/src/auth/auth.constants.ts
@@ -1,16 +1,1 @@
-export const USER_ALREADY_REGISTERED_WITH_EMAIL_AND_LOGIN =
-  '❌ Пользователь с таким email и login уже зарегистрирован.';
-export const USER_ALREADY_REGISTERED_WITH_EMAIL =
-  '❌ Пользователь с таким email уже зарегистрирован.';
-export const USER_ALREADY_REGISTERED_WITH_LOGIN =
-  '❌ Пользователь с таким login уже зарегистрирован.';
-export const BOTH_EMAIL_AND_LOGIN_ERROR =
-  '❌ Укажите только один параметр: email или login.';
-
 export const USER_INVALID_PASSWORD = '❌ Неверный пароль';
-export const NOT_FOUND_2FA_CODE = '❌ Требуется код 2FA';
-
-export const CONFIRM_EMAIL_TOKEN_INVALID = '❌ Неверный или устаревший токен';
-export const CONFIRM_EMAIL_TOKEN_SUCCESS = '✅ Email подтверждён';
-export const CONFIRM_EMAIL_TOKEN_GENERATE =
-  '🧐 Проверьте почту для подтверждения';
