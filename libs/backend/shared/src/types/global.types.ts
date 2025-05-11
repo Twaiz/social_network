f9dd8467d31383b8b4a649285f02c6b175290f8b
Twaiz <homenkobodya@gmail.com>
2025-05-11 11:14:47 +0300

    remove[lib - types]

--- libs/backend/shared/src/types/global.types.ts
+++
@@ -1,7 +0,0 @@
-import { Type, DynamicModule, ForwardReference } from '@nestjs/common';
-
-export type IEntryNestModule<T> =
-  | Type<T>
-  | DynamicModule
-  | ForwardReference
-  | Promise<Type<T> | DynamicModule | ForwardReference>;
