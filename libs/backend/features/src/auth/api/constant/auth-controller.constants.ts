7820e5fe2dea93d20b2165d2af8b59f069302a62
Twaiz <homenkobodya@gmail.com>
2025-05-11 09:51:08 +0300

    update[auth-controller.constants]: removed someone constants

---
+++ libs/backend/features/src/auth/api/constant/auth-controller.constants.ts
@@ -0,0 +1,12 @@
+export const USER_ALREADY_REGISTERED_WITH_EMAIL_AND_LOGIN =
+  '❌ Пользователь с таким email и login уже зарегистрирован.';
+export const USER_ALREADY_REGISTERED_WITH_EMAIL =
+  '❌ Пользователь с таким email уже зарегистрирован.';
+export const USER_ALREADY_REGISTERED_WITH_LOGIN =
+  '❌ Пользователь с таким login уже зарегистрирован.';
+export const BOTH_EMAIL_AND_LOGIN_ERROR =
+  '❌ Укажите только один параметр: email или login.';
+
+export const CONFIRM_EMAIL_TOKEN_SUCCESS = '✅ Email подтверждён';
+export const CONFIRM_EMAIL_TOKEN_GENERATE =
+  '🧐 Проверьте почту для подтверждения';
