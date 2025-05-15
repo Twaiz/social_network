58f50aed43dcee93c4e10603fc1c4105e65dabf2
Twaiz <homenkobodya@gmail.com>
2025-05-15 08:47:01 +0300

    update[emailConfirm.guard]: added checks and improved code

--- libs/backend/shared/structure/guards/emailConfirm.guard.ts
+++ libs/backend/shared/structure/guards/emailConfirm.guard.ts
@@ -1,10 +1,30 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
+
+import { AuthenticatedRequest } from '../types';
+import { USER_NOT_FOUND } from '../../config';
+import { EMAIL_NOT_CONFIRMED_ERROR } from './constant/emailConfirm.constants';
 
 @Injectable()
 export class EmailConfirmGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
-    const { user } = context.switchToHttp().getRequest();
+    const request: AuthenticatedRequest = context.switchToHttp().getRequest();
+    const user = request.user;
 
-    return user?.isEmailConfirm === true;
+    if (!user) {
+      Logger.error(USER_NOT_FOUND, 'EmailConfirmGuard');
+      throw new BadRequestException(USER_NOT_FOUND);
+    }
+
+    if (!user.isEmailConfirm) {
+      throw new BadRequestException(EMAIL_NOT_CONFIRMED_ERROR);
+    }
+
+    return true;
   }
 }
