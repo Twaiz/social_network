67e2b9e74f16ec929ca6b55c8e1ac8a51eb1c3c1
Twaiz <homenkobodya@gmail.com>
2025-05-16 13:51:51 +0300

    update[main.ts]: added import UserModule and launch server

--- apps/backend/user/src/main.ts
+++ apps/backend/user/src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrap, GetEnv } from '@shared';
+import { UserModule } from '@features/user';
 
 const userPort = process.env.USER_SERVER_PORT;
 const port = GetEnv.getServerPort(userPort);
 
-// bootstrap()
+bootstrap<UserModule>(UserModule, port);
