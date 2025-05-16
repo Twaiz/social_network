6c0a2588ab447304dc375730ccc97731371702cf
Twaiz <homenkobodya@gmail.com>
2025-05-16 21:21:53 +0300

    update[auth-controller.constants and global.constants]: constant moved to global.constants

--- libs/backend/features/auth/api/constant/auth-controller.constants.ts
+++ libs/backend/features/auth/api/constant/auth-controller.constants.ts
@@ -2,8 +2,6 @@ export const USER_ALREADY_REGISTERED_WITH_EMAIL_AND_LOGIN =
   '❌ Пользователь с таким email и login уже зарегистрирован.';
 export const USER_ALREADY_REGISTERED_WITH_EMAIL =
   '❌ Пользователь с таким email уже зарегистрирован.';
-export const USER_ALREADY_REGISTERED_WITH_LOGIN =
-  '❌ Пользователь с таким login уже зарегистрирован.';
 export const BOTH_EMAIL_AND_LOGIN_ERROR =
   '❌ Укажите только один параметр: email или login.';
 
--- libs/backend/shared/config/constants/global.constants.ts
+++ libs/backend/shared/config/constants/global.constants.ts
@@ -1,4 +1,6 @@
 export const USER_NOT_FOUND = '❌ Пользователь не найден';
+export const USER_ALREADY_REGISTERED_WITH_LOGIN =
+  '❌ Пользователь с таким login уже зарегистрирован.';
 
 export const APP_INIT_FAILED =
   '❌ Не удалось инициализировать приложение: bootstrap вернул null/undefined';
