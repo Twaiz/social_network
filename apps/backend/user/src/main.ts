db311d52c51007c9291196a052bf9a3a1e47a646
Twaiz <homenkobodya@gmail.com>
2025-05-16 13:39:38 +0300

    update[user main.ts]: added settings for launch server

--- apps/backend/user/src/main.ts
+++ apps/backend/user/src/main.ts
@@ -1,21 +1,6 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import { bootstrap, GetEnv } from '@shared';
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+const userPort = process.env.USER_SERVER_PORT;
+const port = GetEnv.getServerPort(userPort);
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
-}
-
-bootstrap();
+// bootstrap()
