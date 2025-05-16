de06f2386f1a50ccb965ba54866f9547d1ab63fe
Twaiz <homenkobodya@gmail.com>
2025-05-16 13:48:51 +0300

    update[user.module]: updated module settings

--- libs/backend/features/user/model/user.module.ts
+++ libs/backend/features/user/model/user.module.ts
@@ -1,13 +1,12 @@
-import { UserSchema } from '@entities/user';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
-import { PassportModule } from '@nestjs/passport';
 
-import { connectToMongoDB, getJwtConfig, JwtStrategy } from '@shared';
 import { UserController } from '../api';
 import { UserService } from './user.service';
+
+import { connectToMongoDB, JwtStrategy } from '@shared';
+import { UserSchema } from '@entities/user';
 
 @Module({
   imports: [
@@ -18,14 +17,9 @@ import { UserService } from './user.service';
       useFactory: connectToMongoDB,
     }),
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: getJwtConfig,
-    }),
-    PassportModule,
   ],
   controllers: [UserController],
   providers: [UserService, JwtStrategy],
+  exports: [UserService],
 })
 export class UserModule {}
