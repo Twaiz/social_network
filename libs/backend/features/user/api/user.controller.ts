d07fedd07d31cb159416e02e0907e8fbe8411d3e
Twaiz <homenkobodya@gmail.com>
2025-05-18 13:24:32 +0300

    update[user.controller]: added route 'updateUserInfo'

--- libs/backend/features/user/api/user.controller.ts
+++ libs/backend/features/user/api/user.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, Post, Req, UseGuards } from '@nestjs/common';
+
+import { UserService } from '../model';
+import { NewUserInfoCredentialsDto } from '../dto';
+import { type AuthenticatedRequest, IUser, JwtAuthGuard } from '@shared';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @HttpCode(200)
+  @UseGuards(JwtAuthGuard)
+  @Post('update')
+  async updateUserInfo(
+    @Req() req: AuthenticatedRequest,
+    newUserInfoCredentialsDto: NewUserInfoCredentialsDto,
+  ): Promise<IUser> {
+    const user = req.user;
+
+    return this.userService.updateUserInfo(user, newUserInfoCredentialsDto);
+  }
+}
