49eff24155742e2a2dadf77b042e7a7656bb5f8f
Twaiz <homenkobodya@gmail.com>
2025-05-19 16:00:47 +0300

    add[getActiveToken]

---
+++ libs/backend/shared/config/get-active-token/getActiveToken.ts
@@ -0,0 +1,42 @@
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Model } from 'mongoose';
+import { compareSync } from 'bcryptjs';
+
+import { findUserByLogin } from '../../api';
+import { GetEnv } from '../../kernel';
+import { getJwtToken } from '../../lib';
+import { IUser } from '../../structure';
+
+import { USER_NOT_FOUND } from '../constants';
+import { USER_PASSWORD_INVALID } from './constant';
+
+export const getActiveToken = async (
+  jwtService: JwtService,
+  configService: ConfigService,
+  userModel: Model<IUser>,
+): Promise<string> => {
+  const jwtSecret = GetEnv.getJwtSecret(configService);
+  const jwtExpiresIn = GetEnv.getJwtExpiresIn(configService);
+
+  const user = await findUserByLogin(userModel, 'admin');
+  if (!user) {
+    Logger.error(USER_NOT_FOUND);
+    process.exit(1);
+  }
+
+  const isPasswordValid = compareSync('admin123', user.passwordHash);
+  if (!isPasswordValid) {
+    Logger.error(USER_PASSWORD_INVALID);
+    process.exit(1);
+  }
+
+  const token = await getJwtToken(jwtService, {
+    user,
+    jwtSecret,
+    jwtExpiresIn,
+  });
+
+  return token;
+};
