bbfae28437e5fc48d287942e55d3d60aae91c6b5
Twaiz <homenkobodya@gmail.com>
2025-05-19 16:04:04 +0300

    update[user.spec]: added basic setup for writing tests

--- e2e/backend/user-e2e/src/user/user.spec.ts
+++ e2e/backend/user-e2e/src/user/user.spec.ts
@@ -1,10 +1,43 @@
-import axios from 'axios';
+import { ConfigService } from '@nestjs/config';
+import { INestApplication, Logger } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { App } from 'supertest/types';
 
-describe('GET /api', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/api`);
+import {
+  APP_INIT_FAILED,
+  bootstrap,
+  getActiveToken,
+  GetEnv,
+  IUser,
+} from '@shared';
+import { UserModule } from '@features/user';
 
-    expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
+describe('App - User (e2e)', () => {
+  let app: INestApplication<App>;
+  let token: string;
+
+  let jwtService: JwtService;
+  let configService: ConfigService;
+  let userModel: Model<IUser>;
+
+  beforeAll(async () => {
+    const userPort = process.env.USER_SERVER_PORT;
+    const port = GetEnv.getServerPort(userPort);
+
+    const serverApp = await bootstrap<App>(UserModule, port);
+    if (!serverApp) {
+      Logger.error(APP_INIT_FAILED);
+      process.exit(1);
+    }
+
+    app = serverApp;
+
+    jwtService = app.get(JwtService);
+    configService = app.get(ConfigService);
+    userModel = app.get(getModelToken('User'));
+
+    token = await getActiveToken(jwtService, configService, userModel);
   });
 });
