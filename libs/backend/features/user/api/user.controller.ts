9effa176173410b08d2cbf3d25b1e31e077660a2
Twaiz <homenkobodya@gmail.com>
2025-05-20 11:07:30 +0300

    update[user.controller]: added method for changeEmail

--- libs/backend/features/user/api/user.controller.ts
+++ libs/backend/features/user/api/user.controller.ts
@@ -9,13 +9,18 @@ import {
 } from '@nestjs/common';
 
 import { UserService } from '../model/user.service';
-import { EMPTY_DTO } from './constant/user-controller.constants';
+import {
+  CHANGE_EMAIL_GENERATE,
+  EMPTY_DTO,
+} from './constant/user-controller.constants';
 import {
   type AuthenticatedRequest,
+  EmailConfirmGuard,
   IUser,
   JwtAuthGuard,
   NewUserInfoCredentialsDto,
 } from '@shared';
+import { ChangeEmailCredentialsDto } from '../dto';
 
 @Controller('user')
 export class UserController {
@@ -35,5 +40,19 @@ export class UserController {
     const user = req.user;
 
     return this.userService.updateUserInfo(user, newUserInfoCredentialsDto);
+  }
+
+  @HttpCode(201)
+  @Post('change-email')
+  @UseGuards(JwtAuthGuard, EmailConfirmGuard)
+  async changeEmail(
+    @Req() req: AuthenticatedRequest,
+    @Body() changeEmailCredentialsDto: ChangeEmailCredentialsDto,
+  ): Promise<{ message: string }> {
+    const user = req.user;
+
+    await this.userService.changeEmail(user, changeEmailCredentialsDto);
+
+    return { message: CHANGE_EMAIL_GENERATE };
   }
 }
