2d4125133b219f9860084c3e6f35f697e7103584
Twaiz <homenkobodya@gmail.com>
2025-05-20 09:59:40 +0300

    add[sendEmail]

--- libs/backend/shared/kernel/lib/index.ts
+++ libs/backend/shared/kernel/lib/index.ts
@@ -1,2 +1,3 @@
 export * from './bootstrap';
 export * from './get-env';
+export * from './sendEmail';
---
+++ libs/backend/shared/kernel/lib/sendEmail/sendEmail.ts
@@ -0,0 +1,24 @@
+import { ConfigService } from '@nestjs/config';
+import nodemailer from 'nodemailer';
+
+export const sendEmailConfirmation = async (
+  configService: ConfigService,
+  subject: string,
+  htmlContent: string,
+  email: string,
+): Promise<void> => {
+  const transporter = nodemailer.createTransport({
+    host: configService.get<string>('MAIL_HOST'),
+    port: configService.get<number>('MAIL_PORT'),
+    auth: {
+      user: configService.get<string>('MAIL_USERNAME'),
+      pass: configService.get<string>('MAIL_PASSWORD'),
+    },
+  });
+
+  await transporter.sendMail({
+    to: email,
+    subject,
+    html: htmlContent,
+  });
+};
