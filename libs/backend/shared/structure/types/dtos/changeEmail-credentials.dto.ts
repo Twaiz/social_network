7022a95125dea345206682024c5636fcba96abfa
Twaiz <homenkobodya@gmail.com>
2025-05-20 11:24:55 +0300

    update[changeEmail-credentials.dto]: added new setting for best checks

--- libs/backend/shared/structure/types/dtos/changeEmail-credentials.dto.ts
+++ libs/backend/shared/structure/types/dtos/changeEmail-credentials.dto.ts
@@ -1,7 +1,8 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class ChangeEmailCredentialsDto {
   @IsString()
   @IsNotEmpty()
+  @IsEmail()
   newEmail!: string;
 }
