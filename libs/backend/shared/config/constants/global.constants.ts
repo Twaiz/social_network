0ac92df150c92ae0d6fae9fa9d9a704257d968df
Twaiz <homenkobodya@gmail.com>
2025-05-20 09:53:23 +0300

    update[auth-controller.constants]: one constant moved

--- libs/backend/shared/config/constants/global.constants.ts
+++ libs/backend/shared/config/constants/global.constants.ts
@@ -1,6 +1,8 @@
 export const USER_NOT_FOUND = '❌ Пользователь не найден';
 export const USER_ALREADY_REGISTERED_WITH_LOGIN =
   '❌ Пользователь с таким login уже зарегистрирован.';
+export const USER_ALREADY_REGISTERED_WITH_EMAIL =
+  '❌ Пользователь с таким email уже зарегистрирован.';
 
 export const APP_INIT_FAILED =
   '❌ Не удалось инициализировать приложение: bootstrap вернул null/undefined';
