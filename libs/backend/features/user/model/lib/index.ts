bbf01d6045ad5a1fc1278cb6572efea5f0b8b7cf
Twaiz <homenkobodya@gmail.com>
2025-05-21 16:03:15 +0300

    add[sendEmailConfirmNewPassword]

--- libs/backend/features/user/model/lib/index.ts
+++ libs/backend/features/user/model/lib/index.ts
@@ -1,2 +1,3 @@
 export * from './sendEmailConfirmNewEmail';
 export * from './sendEmailChangePassword';
+export * from './sendEmailConfirmNewPassword';
---
+++ libs/backend/features/user/model/lib/sendEmailConfirmNewPassword.ts
@@ -0,0 +1,21 @@
+import { ConfigService } from '@nestjs/config';
+import { sendEmail } from '@shared';
+
+export const sendEmailConfirmNewPassword = async (
+  configService: ConfigService,
+  email: string,
+  fullName: string,
+): Promise<void> => {
+  const htmlContent = `
+    <p>Здравствуйте, ${fullName}!</p>
+    <p>Ваш пароль был успешно изменён.</p>
+    <p>Если вы не выполняли это действие, немедленно свяжитесь со службой поддержки для обеспечения безопасности вашей учётной записи.</p>
+  `;
+
+  await sendEmail(
+    configService,
+    'Подтверждение изменения пароля',
+    htmlContent,
+    email,
+  );
+};
