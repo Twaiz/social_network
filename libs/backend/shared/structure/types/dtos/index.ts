1a0bc744300e7def05ef6f8b7444d8747ec7ac49
Twaiz <homenkobodya@gmail.com>
2025-05-21 15:51:15 +0300

    add[confirmNewPassword-credentials.dto]

--- libs/backend/shared/structure/types/dtos/index.ts
+++ libs/backend/shared/structure/types/dtos/index.ts
@@ -2,3 +2,4 @@ export * from './newUserInfo-credentials.dto';
 export * from './changeEmail-credentials.dto';
 export * from './confirmChangedEmail-credentials.dto';
 export * from './changePassword-credentials.dto';
+export * from './confirmNewPassword-credentials.dto';
