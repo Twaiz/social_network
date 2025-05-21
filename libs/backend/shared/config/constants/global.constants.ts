5c06392e444ad7855912602d6f4de6ab89f66db0
Twaiz <homenkobodya@gmail.com>
2025-05-21 15:05:59 +0300

    update[auth.constants, getActiveToken.constants]: removed this files and moved constant to global.constants

--- libs/backend/shared/config/constants/global.constants.ts
+++ libs/backend/shared/config/constants/global.constants.ts
@@ -1,4 +1,5 @@
 export const USER_NOT_FOUND = '❌ Пользователь не найден';
+export const USER_PASSWORD_INVALID = '❌ Неверный пароль';
 export const USER_ALREADY_REGISTERED_WITH_LOGIN =
   '❌ Пользователь с таким login уже зарегистрирован.';
 export const USER_ALREADY_REGISTERED_WITH_EMAIL =
