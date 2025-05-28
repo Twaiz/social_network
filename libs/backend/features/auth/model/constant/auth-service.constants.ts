2e02f3c3ca4967bf67e4440887fd6b65d45acb64
Twaiz <homenkobodya@gmail.com>
2025-05-28 17:27:20 +0300

    update[auth-service.constants]: added constants for not found passwordHash

--- libs/backend/features/auth/model/constant/auth-service.constants.ts
+++ libs/backend/features/auth/model/constant/auth-service.constants.ts
@@ -1,2 +1,3 @@
 export const NOT_FOUND_2FA_CODE = '❌ Требуется код 2FA';
 export const CONFIRM_EMAIL_TOKEN_INVALID = '❌ Неверный или устаревший токен';
+export const PASSWORDHASH_IS_NOT_FOUND = '❌ PasswordHash не найден';
