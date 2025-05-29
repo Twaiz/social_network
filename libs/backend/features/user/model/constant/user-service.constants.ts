bf47540b8182a80f8b8c4b1c2fdd6e7eee231e37
Twaiz <homenkobodya@gmail.com>
2025-05-29 14:27:26 +0300

    update[user-service.constants]: old constant returned

--- libs/backend/features/user/model/constant/user-service.constants.ts
+++ libs/backend/features/user/model/constant/user-service.constants.ts
@@ -1,2 +1,5 @@
 export const CONFIRM_CHANGE_TOKEN_INVALID = '❌ Неверный или устаревший токен';
 export const CHANGE_EMAIL_TOKEN_NOT_FOUND = '❌ Новый email не найдено';
+
+export const IDENTICAL_PASSWORD =
+  '❌ Новый password не может совпадать с текущим';
