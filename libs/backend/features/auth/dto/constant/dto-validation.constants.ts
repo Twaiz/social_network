c920be1bb67a42567dc8a4fd2a9227c74e198866
Twaiz <homenkobodya@gmail.com>
2025-05-29 15:53:35 +0300

    update[dto-validation.constants.ts]: added new 2 constants for password

--- libs/backend/features/auth/dto/constant/dto-validation.constants.ts
+++ libs/backend/features/auth/dto/constant/dto-validation.constants.ts
@@ -2,3 +2,8 @@ export const LOGIN_MIN_LENGTH_ERROR =
   '❌ Логин должен содержать минимум 4 символа.';
 export const LOGIN_MAX_LENGTH_ERROR =
   '❌ Логин не должен превышать 16 символов.';
+
+export const PASSWORD_MIN_LENGTH_ERROR =
+  '❌ Пароль должен содержать минимум 6 символов.';
+export const PASSWORD_MAX_LENGTH_ERROR =
+  '❌ Пароль не должен превышать 32 символа.';
