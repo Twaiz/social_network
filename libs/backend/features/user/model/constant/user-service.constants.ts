9eea009af2351fd51bb7edcc0e68919610456ef2
Twaiz <homenkobodya@gmail.com>
2025-05-29 14:19:54 +0300

    update[user-service.constants]: removed unnecessary constants

--- libs/backend/features/user/model/constant/user-service.constants.ts
+++ libs/backend/features/user/model/constant/user-service.constants.ts
@@ -1,7 +1,2 @@
-export const IDENTICAL_EMAIL = '❌ Новый email не может совпадать с текущим';
-export const IDENTICAL_LOGIN = '❌ Новый login не может совпадать с текущим';
-export const IDENTICAL_PASSWORD =
-  '❌ Новый password не может совпадать с текущим';
-
 export const CONFIRM_CHANGE_TOKEN_INVALID = '❌ Неверный или устаревший токен';
 export const CHANGE_EMAIL_TOKEN_NOT_FOUND = '❌ Новый email не найдено';
