b53fb3b411c18fd71ac17f0041d82148ae7a61de
Twaiz <homenkobodya@gmail.com>
2025-06-02 18:05:17 +0300

    update[user.queries]: refactor all functions

--- libs/backend/shared/api/user.queries.ts
+++ libs/backend/shared/api/user.queries.ts
@@ -1,20 +1,33 @@
+import { BadRequestException, Logger } from '@nestjs/common';
 import { Model } from 'mongoose';
 
-const findUserByEmail = async <T>(
-  model: Model<T>,
-  email: string,
-  select?: string,
-): Promise<T | null> => {
-  return model.findOne({ email }).select(select || '');
+const findUser = {
+  async byEmail<T>(
+    model: Model<T>,
+    email: string,
+    errorMessage: string,
+    select?: string,
+  ): Promise<null | T> {
+    const user = await model.findOne({ email }).select(select || '');
+    if (!user) {
+      Logger.error(errorMessage, 'UserQueries - findUserByEmail');
+      throw new BadRequestException(errorMessage);
+    }
+    return user;
+  },
+  async byLogin<T>(
+    model: Model<T>,
+    login: string,
+    errorMessage: string,
+    select?: string,
+  ): Promise<null | T> {
+    const user = await model.findOne({ login }).select(select || '');
+    if (!user) {
+      Logger.error(errorMessage, 'UserQueries - findUserByLogin');
+      throw new BadRequestException(errorMessage);
+    }
+    return user;
+  },
 };
 
-//* Find User By Login *//
-const findUserByLogin = async <T>(
-  model: Model<T>,
-  login: string,
-  select?: string,
-): Promise<T | null> => {
-  return model.findOne({ login }).select(select || '');
-};
-
-export { findUserByEmail, findUserByLogin };
+export { findUser };
