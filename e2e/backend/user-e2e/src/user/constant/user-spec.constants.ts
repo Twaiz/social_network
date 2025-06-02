5589a8f29d1ed85948e59ce0e973a9f86f5a5e87
Twaiz <homenkobodya@gmail.com>
2025-06-02 19:38:09 +0300

    update[user-spec.constants]: added new 2 constants

--- e2e/backend/user-e2e/src/user/constant/user-spec.constants.ts
+++ e2e/backend/user-e2e/src/user/constant/user-spec.constants.ts
@@ -1,1 +1,3 @@
-export const CHANGE_EMAIL_TOKEN_NOT_FOUND = '❌ Не найден changeEmailToken';
+export const CHANGE_EMAIL_EXPIRES_NOT_FOUND = '❌ Не найден changeEmailExpires';
+export const CHANGE_PASSWORD_EXPIRES_NOT_FOUND =
+  '❌ Не найден changePasswordExpires';
