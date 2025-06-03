a419065eedc9ee80bd80b776d753ed19a63519e5
Twaiz <homenkobodya@gmail.com>
2025-06-03 13:13:04 +0300

    update[verifyTwoFactorCode.ts]: improved code

--- libs/backend/shared/lib/two-factor/verifyTwoFactorCode.ts
+++ libs/backend/shared/lib/two-factor/verifyTwoFactorCode.ts
@@ -1,10 +1,15 @@
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, Logger } from '@nestjs/common';
 import { authenticator } from 'otplib';
 
 import { IUser } from '../../structure';
-import { INVALID_2FA_CODE } from './constant';
+import { INVALID_2FA_CODE, TWO_FACTOR_SECRET_NOT_FOUND } from './constant';
 
 export const verifyTwoFactorCode = (user: IUser, code: string): void => {
+  if (!user.twoFactorSecret) {
+    Logger.error(TWO_FACTOR_SECRET_NOT_FOUND, 'verifyTwoFactorCode');
+    throw new BadRequestException(TWO_FACTOR_SECRET_NOT_FOUND);
+  }
+
   const isTwoFactorCodeValid = authenticator.verify({
     token: code,
     secret: user.twoFactorSecret,
