c682a3d9fbe237e2897b39f15ab1f613594c647b
Twaiz <homenkobodya@gmail.com>
2025-06-03 13:26:43 +0300

    update[auth-controller.constants]: removed constant

--- libs/backend/features/auth/api/constant/auth-controller.constants.ts
+++ libs/backend/features/auth/api/constant/auth-controller.constants.ts
@@ -1,5 +1,3 @@
-export const USER_ALREADY_REGISTERED_WITH_EMAIL_AND_LOGIN =
-  '❌ Пользователь с таким email и login уже зарегистрирован.';
 export const BOTH_EMAIL_AND_LOGIN_ERROR =
   '❌ Укажите только один параметр: email или login.';
 
