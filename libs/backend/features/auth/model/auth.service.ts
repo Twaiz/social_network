fb19668e8e252aaa0484d920acd3156bfbc08702
Twaiz <homenkobodya@gmail.com>
2025-06-03 13:48:23 +0300

    update[auth.service]: removed TODO

--- libs/backend/features/auth/model/auth.service.ts
+++ libs/backend/features/auth/model/auth.service.ts
@@ -188,8 +188,6 @@ export class AuthService {
     if (!userByConfirmEmail) {
       throw new BadRequestException(CONFIRM_EMAIL_TOKEN_INVALID);
     }
-
-    //TODO - добавить сообщения на почту об успешном подтверждение и прочем, как в changeEmail
   }
 
   //* Generate Email Token *//
