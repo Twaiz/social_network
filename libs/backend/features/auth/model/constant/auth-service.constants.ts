b539ff12025e8b22867590487818f44bac3e7fce
Twaiz <homenkobodya@gmail.com>
2025-06-03 13:33:01 +0300

    update[auth-service.constants]: added new constant

--- libs/backend/features/auth/model/constant/auth-service.constants.ts
+++ libs/backend/features/auth/model/constant/auth-service.constants.ts
@@ -1,2 +1,5 @@
 export const NOT_FOUND_2FA_CODE = '❌ Требуется код 2FA';
 export const CONFIRM_EMAIL_TOKEN_INVALID = '❌ Неверный или устаревший токен';
+
+export const USER_ALREADY_REGISTERED_WITH_EMAIL_AND_LOGIN =
+  '❌ Пользователь с таким email и login уже зарегистрирован.';
