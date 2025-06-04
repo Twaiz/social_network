4f58bddeedcc17472669051abbf82de9edb18dc2
Twaiz <homenkobodya@gmail.com>
2025-06-04 13:09:57 +0300

    update[structure/dtos index.ts]: added re-export verifyPassword-credentials.dto

--- libs/backend/shared/structure/types/dtos/index.ts
+++ libs/backend/shared/structure/types/dtos/index.ts
@@ -3,3 +3,4 @@ export * from './changeEmail-credentials.dto';
 export * from './confirmChangedEmail-credentials.dto';
 export * from './changePassword-credentials.dto';
 export * from './confirmNewPassword-credentials.dto';
+export * from './verifyPassword-credentials.dto';
