1ef8887b948a89eae98ae340620b95ae754f573d
Twaiz <homenkobodya@gmail.com>
2025-06-04 13:09:06 +0300

    update[verifyPassword-credentials.dto.ts]: moved to structure/dtos

--- libs/backend/features/auth/dto/index.ts
+++ libs/backend/features/auth/dto/index.ts
@@ -1,4 +1,3 @@
 export * from './confirmEmail-credentials.dto';
 export * from './register-credentials.dto';
 export * from './login-credentials.dto';
-export * from './verifyPassword-credentials.dto';
