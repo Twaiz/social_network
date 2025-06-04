4dc6edbbaea615bfef0c54b09bb3481a9c757097
Twaiz <homenkobodya@gmail.com>
2025-06-04 12:58:36 +0300

    update[verifyPassword.guard.ts]: improved code

--- libs/backend/shared/structure/guards/verifyPassword.guard.ts
+++ libs/backend/shared/structure/guards/verifyPassword.guard.ts
@@ -1,6 +1,7 @@
 import {
   CanActivate,
   ExecutionContext,
+  Logger,
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
@@ -14,16 +15,23 @@ export class VerifyPasswordGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
 
   canActivate(context: ExecutionContext): boolean {
+    const loggerContext = 'VerifyPasswordGuard';
+
     const req = context.switchToHttp().getRequest();
     const verifyPasswordToken = req.headers['x-verification-password-token'];
 
     if (!verifyPasswordToken) {
+      Logger.error(NONE_VERIFICATION_PASSWORD, loggerContext);
       throw new UnauthorizedException(NONE_VERIFICATION_PASSWORD);
     }
 
     try {
       const payload = this.jwtService.verify(verifyPasswordToken);
       if (payload.type !== 'verificationPassword') {
+        Logger.error(
+          PAYLOAD_TYPE_VERIFICATION_PASSWORD_TOKEN_INVALID,
+          loggerContext,
+        );
         throw new UnauthorizedException(
           PAYLOAD_TYPE_VERIFICATION_PASSWORD_TOKEN_INVALID,
         );
@@ -32,7 +40,9 @@ export class VerifyPasswordGuard implements CanActivate {
       req.verifiedUserId = payload.sub;
 
       return true;
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (_error) {
+      Logger.error(VERIFICATION_PASSWORD_INVALID, loggerContext);
       throw new UnauthorizedException(VERIFICATION_PASSWORD_INVALID);
     }
   }
